606b2da2102949ef01f15a6e4f4fe9875c526d25
sailingnumbers <72826427+sailingnumbers@users.noreply.github.com>
2022-07-08 23:15:14 +0000

    Changes to be committed: 	new file:   test/benchmarks.jl 	new file:   test/blas.jl 	new file:   test/machine.jl 	modified:   test/runtests.jl 	new file:   test/thread_pinning.jl 	modified:   test/versions.jl

--- test/versions.jl
+++ test/versions.jl
@@ -1,16 +1,23 @@
 # versions.jl
 
-@info "Check julia version"
+println("
+#############################################
+# CHECK JULIA VERSION
+#############################################")
 using InteractiveUtils
   InteractiveUtils.versioninfo()
   @info "Sys.BINDIR" Sys.BINDIR
   @info "Sys.STDLIB" Sys.STDLIB
-  
-@info "Check versions of packages"
-  using Pkg
+
+println("
+#############################################
+# CHECK VERSIONS OF PACKAGES
+#############################################")
+using Pkg
   @info "Pkg.status()" Pkg.status("BenchmarkTools")
   @info "Pkg.status()" Pkg.status("Hwloc")
   @info "Pkg.status()" Pkg.status("ThreadPinning")
   @info "Pkg.status()" Pkg.status("STREAMBenchmark")
   @info "Pkg.status()" Pkg.status("BandwidthBenchmark")
 
+println("")
