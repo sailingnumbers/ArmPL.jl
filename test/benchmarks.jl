23423bb4bff313e4eb40e846f5620f4194b51255
sailingnumbers <72826427+sailingnumbers@users.noreply.github.com>
2022-07-10 23:16:15 +0000

    modified:   test/benchmarks.jl 	modified:   test/thread_pinning.jl

--- test/benchmarks.jl
+++ test/benchmarks.jl
@@ -9,16 +9,12 @@ using BenchmarkTools
 @info "clear GC" GC.gc()
 
 println(cray_fg_color(r, g, b),"
-#############################################
-# BLAS BENCHMARKS - peakflops
-#############################################")
+# BLAS BENCHMARKS - peakflops")
 @info "pekflops() ", peakflops()
 @info "clear GC" GC.gc()
 
 println(cray_fg_color(r, g, b),"
-#############################################
-# BLAS BENCHMARKS - matrix 1000 x 1000
-#############################################")
+# BLAS BENCHMARKS - matrix 1000 x 1000")
 A = rand(1000,1000); B = rand(1000,1000);
 # to do: print results
 benchmark = @benchmark $A * $B;
@@ -26,9 +22,7 @@ display(benchmark)
 @info "clear GC" GC.gc()
 
 println(cray_fg_color(r, g, b),"
-#############################################
-# BLAS BENCHMARKS - matrix 10000 x 10000
-#############################################")
+# BLAS BENCHMARKS - matrix 10000 x 10000")
 C = rand(10000,10000); D = rand(10000,10000);
 # to do: print results
 benchmark = @benchmark $C * $D;
@@ -36,9 +30,7 @@ display(benchmark)
 @info "clear GC" GC.gc()
 
 println(cray_fg_color(r, g, b),"
-#############################################
-# BLAS BENCHMARKS - axpy!
-#############################################")
+# BLAS BENCHMARKS - axpy!")
 n = [10^k for k in 1:6]
 x, y = [rand(ni) for ni in n], [rand(ni) for ni in n]
 α = sqrt(pi)
@@ -60,16 +52,12 @@ println(cray_fg_color(r, g, b),"
 using BandwidthBenchmark
 
 println(cray_fg_color(r, g, b),"
-##################################################################
-# BANDWIDTHBENCHMARK - memory bandwidth using streaming kernels
-##################################################################")
+# BANDWIDTHBENCHMARK - memory bandwidth using streaming kernels")
 print(BandwidthBenchmark.bwbench(#= ; verbose=true =#))
 @info "clear GC" GC.gc()
 
 println(cray_fg_color(r, g, b),"
-#######################################################################
-# BANDWIDTHBENCHMARK - memory bandwidth - increasing number of threads
-#######################################################################")
+# BANDWIDTHBENCHMARK - memory bandwidth - increasing number of threads")
 df = DataFrame(BandwidthBenchmark.bwscaling(#= ; verbose=true =#), :auto)
 print(UnicodePlots.lineplot(df[!,:x1], df[!,:x2], title = "Memory bandwidth (bwscaling) - (1:max_nthreads) [lineplot])", xlabel = "# of cores", ylabel = "MFlops/s", border=:dotted))
 print(UnicodePlots.barplot(
@@ -80,10 +68,8 @@ print(UnicodePlots.barplot(
 @info "clear GC" GC.gc()
 
 println(cray_fg_color(r, g, b),"
-#################################################################################
 # BANDWIDTHBENCHMARK - floating point performance - increasing number of threads
-# (triad kernel based scaling)
-#################################################################################")
+# (triad kernel based scaling)")
 df = DataFrame(BandwidthBenchmark.flopsscaling(), :auto)
 print(UnicodePlots.lineplot(
         df[!,:x1], df[!,:x2], 
@@ -104,24 +90,18 @@ println(cray_fg_color(r, g, b),"
 using BandwidthBenchmark
 
 println(cray_fg_color(r, g, b),"
-#######################################################################
-# STREAMBenchmark - maximal memory bandwidth (single and multithreaded)
-#######################################################################")
+# STREAMBenchmark - maximal memory bandwidth (single and multithreaded)")
 using STREAMBenchmark
 @info "Results: Estimating achievable (maximal) memory bandwidth (single and multithreaded)"
 STREAMBenchmark.benchmark()
 
 println(cray_fg_color(r, g, b),"
-#######################################################################
-# STREAMBenchmark - maximal memory wrt the number of threads
-#######################################################################")
+# STREAMBenchmark - maximal memory wrt the number of threads")
 y = STREAMBenchmark.scaling_benchmark()
 print(UnicodePlots.lineplot(1:length(y), y, title = "Bandwidth Scaling", xlabel = "# cores", ylabel = "MB/s", border = :dotted#= , canvas = AsciiCanvas =#))
 @info "GC.gc()" GC.gc() # clear gc
 
 println(cray_fg_color(r, g, b),"
-#######################################################################
 # STREAMBenchmark - vector lengths check
-# (default is four times the size of the outermost cache)
-#######################################################################")
+# (default is four times the size of the outermost cache)")
 STREAMBenchmark.vector_length_dependence()
--- test/thread_pinning.jl
+++ test/thread_pinning.jl
@@ -24,25 +24,19 @@ ThreadPinning.pinthreads(:compact)
 # ThreadPinning.pinthreads(:spread)
 
 println(cray_fg_color(r, g, b),"
-###############################################
-# THREAD PINNING - threadinfo
-###############################################")
+# THREAD PINNING - threadinfo")
 print(ThreadPinning.threadinfo(; blas=true, hints=true, color=true))
 # to check: as blas hints might not be supported ThreadPinning with ArmPL
 
 println()
 
 println(cray_fg_color(r, g, b),"
-#################################################
 # THREAD PINNING - Core 2 core latencies: results
-# TO DO: ┌ Error: Need at least two Julia threads.
-#################################################")
+# TO DO: ┌ Error: Need at least two Julia threads.")
 latencies = ThreadPinning.bench_core2core_latency();
 display(latencies)
 
 println(cray_fg_color(r, g, b),"
-#################################################
-# THREAD PINNING - Core 2 core latencies: plot
-#################################################")
+# THREAD PINNING - Core 2 core latencies: plot")
 print(UnicodePlots.heatmap(latencies, colorbar=true, colormap=:jet))
 @info "GC.gc()" GC.gc() # clear gc
